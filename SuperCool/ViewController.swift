a60c6a34ed1cbc346a67526dec71c66bdb09e2de
Jesus Lopez de Nava <lodena@users.noreply.github.com>
2015-10-03 23:12:46 -0500

    Added files for supercool

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Jesus Lopez de Nava on 9/26/15.
+//  Copyright © 2015 LodenaApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var coolLogo: UIImageView!
+    @IBOutlet var coolBg: UIImageView!
+    @IBOutlet var uncoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func makeMeCool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        uncoolButton.hidden = true
+    }
+
+}
+
